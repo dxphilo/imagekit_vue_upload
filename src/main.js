0a636f499d624e820da280559acf64065bfdf180
John Philip <developerphilo@gmail.com>
2023-11-23 13:57:46 +0300

    fix: remove comments

--- src/main.js
+++ src/main.js
@@ -4,13 +4,11 @@ import ImageKit from "imagekitio-vue"
 
 Vue.config.productionTip = false;
 
-
-// Configure Vue.js to use ImageKit for efficient image and video management.
-// Replace the values below with your ImageKit credentials.
+// Replace the values below with your ImageKit credentials
 Vue.use(ImageKit, {
-  urlEndpoint: "https://ik.imagekit.io/mljbvpxzd", // Required: Your ImageKit URL endpoint
-  publicKey: "public_Z1+OqJvvxckkpW8x45Kpoj5SQP0=", // Optional: Your ImageKit public key
-  authenticationEndpoint: "https://elk2.onrender.com/imagekit" // Optional: Your authentication endpoint
+  urlEndpoint: "https://ik.imagekit.io/mljbvpxzd", 
+  publicKey: "public_Z1+OqJvvxckkpW8x45Kpoj5SQP0=", 
+  authenticationEndpoint: "https://elk2.onrender.com/imagekit"
 });
 
 new Vue({
